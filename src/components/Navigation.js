079b0792391e8d4fdc549a6980d010e894334350
conradjohnson <skaterjames@gmail.com>
2022-11-02 09:23:15 -0500

    fixing logo link

--- src/components/Navigation.js
+++ src/components/Navigation.js
@@ -6,7 +6,7 @@ function Navigation() {
         <>
     <nav className="navbar navbar-expand-lg bg-secondary text-uppercase fixed-top" id="mainNav">
         <div className="container">
-            <a className="navbar-brand" href="#page-top"><img src="/assets/img/logo.png" width="240px" alt="Conrad Johnson Portfolio Logo"/></a>
+            <a className="navbar-brand" href="#page-top"><img src="./assets/img/logo.png" width="240px" alt="Conrad Johnson Portfolio Logo"/></a>
             <button className="navbar-toggler text-uppercase font-weight-bold bg-primary text-white rounded" type="button" data-bs-toggle="collapse" data-bs-target="#navbarResponsive" aria-controls="navbarResponsive" aria-expanded="false" aria-label="Toggle navigation">
                 Minu:
                 <i className="fas fa-bars"></i>
