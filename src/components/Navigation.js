7a26f25a692074b797383d33b71ecb96d81f46aa
conradjohnson <skaterjames@gmail.com>
2022-11-14 23:22:12 -0600

    silly gnpages

--- src/components/Navigation.js
+++ src/components/Navigation.js
@@ -6,17 +6,17 @@ function Navigation() {
         <>
     <nav className="navbar navbar-expand-lg bg-secondary text-uppercase fixed-top" id="mainNav">
         <div className="container">
-            <a className="navbar-brand" href="/"><img src="./assets/img/logo.png" width="240px" alt="Conrad Johnson Portfolio Logo"/></a>
+            <a className="navbar-brand" href="home"><img src="./assets/img/logo.png" width="240px" alt="Conrad Johnson Portfolio Logo"/></a>
             <button className="navbar-toggler text-uppercase font-weight-bold bg-primary text-white rounded" type="button" data-bs-toggle="collapse" data-bs-target="#navbarResponsive" aria-controls="navbarResponsive" aria-expanded="false" aria-label="Toggle navigation">
                 Minu:
                 <i className="fas fa-bars"></i>
             </button>
             <div className="collapse navbar-collapse" id="navbarResponsive">
                 <ul className="navbar-nav ms-auto">
-                    <li className="nav-item mx-0 mx-lg-1"><a className="nav-link py-3 px-0 px-lg-3 rounded" href="/portfolio">Portfolio</a></li>
-                    <li className="nav-item mx-0 mx-lg-1"><a className="nav-link py-3 px-0 px-lg-3 rounded" href="/aboutme">About Me</a></li>
-                    <li className="nav-item mx-0 mx-lg-1"><a className="nav-link py-3 px-0 px-lg-3 rounded" href="/contact">Contact</a></li>
-                    <li className="nav-item mx-0 mx-lg-1"><a className="nav-link py-3 px-0 px-lg-3 rounded" href="/resume">Resume</a></li>
+                    <li className="nav-item mx-0 mx-lg-1"><a className="nav-link py-3 px-0 px-lg-3 rounded" href="portfolio">Portfolio</a></li>
+                    <li className="nav-item mx-0 mx-lg-1"><a className="nav-link py-3 px-0 px-lg-3 rounded" href="aboutme">About Me</a></li>
+                    <li className="nav-item mx-0 mx-lg-1"><a className="nav-link py-3 px-0 px-lg-3 rounded" href="contact">Contact</a></li>
+                    <li className="nav-item mx-0 mx-lg-1"><a className="nav-link py-3 px-0 px-lg-3 rounded" href="resume">Resume</a></li>
                 </ul>
             </div>
         </div>
